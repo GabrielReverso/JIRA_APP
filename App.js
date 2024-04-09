efa2bcef8e2d17a4fd5fd2a4ef17cab9727c3312
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2024-04-08 21:28:12 -0300

    Feat: Teste de customização

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import CustomButton from './src/components/CustomButton';
+import { Alert, StyleSheet, View } from "react-native";
+import CustomButton from "./src/components/CustomButton";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useEffect, useState } from "react";
+import StartComponent from "./src/screens/StartScreen";
+import MyTabs from "./src/components/BottomTab";
 
+const Stack = createStackNavigator();
 
 export default function App() {
-
-  const logMessage = () => {
-    console.log("Oi");
-  };
-
   return (
-    <View style={styles.container}>
-      <CustomButton onPress={logMessage} text={"Teste Componente"}/>
-      <CustomButton onPress={logMessage} text={"Teste Componente 2"}/>
-      <CustomButton onPress={logMessage} text={"Teste Componente 3"}/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ header: () => null }}>
+        <Stack.Screen name="StartScreen" component={StartComponent} />
+        <Stack.Screen name="MainGroup" component={MyTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#001139", //01184D
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
